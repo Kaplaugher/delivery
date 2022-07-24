dc91f9abf3c6d618d8ec9894ad7a9c17302811e6
Kyle <kaplaugher94@gmail.com>
2022-07-24 16:01:23 -0400

    wip

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,6 +1,7 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import React from 'react';
 import { ArrowRightIcon } from 'react-native-heroicons/outline';
+import RestaurantCard from './RestaurantCard';
 
 const FeaturedRow = ({ id, title, description }) => {
   return (
@@ -10,6 +11,24 @@ const FeaturedRow = ({ id, title, description }) => {
         <ArrowRightIcon color='#00CCBB' />
       </View>
       <Text className='text-xs text-gray-500 px-4'>{description}</Text>
+      <ScrollView
+        horizontal
+        contentContainerStyle={{ paddingHorizontal: 15 }}
+        showsHorizontalScrollIndicator={false}
+        className='pt-4'>
+        <RestaurantCard
+          id={123}
+          imgUrl='https://links.papareact.com/gn7'
+          title='Yo sushi'
+          rating={4.5}
+          genre='Japanese'
+          address='213 Main St'
+          short_description='good sushi yo'
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+      </ScrollView>
     </View>
   );
 };
